1e7af5549b8c9b0f30e2414fa3e7e0d8fd066d2f
DisIsGood <rlaxodnjs6574@gmail.com>
2021-05-14 23:35:49 +0900

    05.14 김태원
    1. volley 라이브러리 추가.
    2. 액션바 숨김.
    3. 전체화면
    4. LoginActivity 작성.
    5. LogoActivity 작성.
    6. LogoActivity -> LoginActivity 5초 뒤 이동.
    
    * 이미지 파일 이름 오류때문에 바꿨습니다.
    대문자가 들어가면 안되네요 ㅎㅎ

--- app/src/main/java/com/taewon/dolphin/LoginActivity.java
+++ app/src/main/java/com/taewon/dolphin/LoginActivity.java
@@ -1,13 +1,12 @@
 package com.taewon.dolphin;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/taewon/dolphin/StartActivity.java
@@ -0,0 +1,36 @@
+package com.taewon.dolphin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class StartActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        //임시로 그냥 얻어오기만 했어요.
+        imageView = (ImageView)findViewById(R.id.logo);
+
+        //Handler를 이용해 5초 딜레이를 주고, 액티비티를 이동합니다.
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //딜레이 후 실행할 부분.
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
