f9c472b4fe03c0ab67ea3922f7bf6e68bee38a85
DisIsGood <rlaxodnjs6574@gmail.com>
2021-05-16 06:01:49 +0900

    로그인 구현완료.

--- app/src/main/java/com/taewon/dolphin/LoginActivity.java
+++ app/src/main/java/com/taewon/dolphin/LoginActivity.java
@@ -9,10 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
     EditText userID, userPassword;
@@ -22,6 +31,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        /* Views */
         Intent resisterIntent = getIntent();
         userID = (EditText) findViewById(R.id.userID);
         userPassword = (EditText) findViewById(R.id.userPassword);
@@ -29,14 +40,61 @@ public class LoginActivity extends AppCompatActivity {
         register = (TextView)findViewById(R.id.register);
         find = (TextView)findViewById(R.id.find);
 
+        //만약 유저가 회원가입을 했다면, 자동으로 설정합니다.
         userID.setText(resisterIntent.getStringExtra("userID"));
         userPassword.setText(resisterIntent.getStringExtra("userPassword"));
 
+        /* 회원가입 클릭 시, 바로 회원가입 창으로 넘어갑니다.*/
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Response.Listener<String> responseLinstener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success)
+                            {
+                                Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("userID", jsonObject.getString("userID"));
+                                intent.putExtra("userName", jsonObject.getString("userName"));
+                                intent.putExtra("userMajor", jsonObject.getString("userMajor"));
+                                intent.putExtra("userDept", jsonObject.getString("userDept"));
+                                intent.putExtra("userPhoneNum", jsonObject.getString("userPhoneNum"));
+                                startActivity(intent);
+                                finish();
+                            }
+                            else
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setTitle("우리한텐 없습니다.").setMessage("\t아이디나 비밀번호가 틀렸습니다.\n\t다시 시도해보세요.").setNegativeButton("확인", null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+                UserLogin validateRequest = new UserLogin(userID.getText().toString(), userPassword.getText().toString(), responseLinstener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(validateRequest);
+
+
             }
         });
 
---
+++ app/src/main/java/com/taewon/dolphin/UserLogin.java
@@ -0,0 +1,26 @@
+package com.taewon.dolphin;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserLogin extends StringRequest {
+
+    private final static String URL = "http://xodnjs2546.cafe24.com/userLogin.php";
+    private Map<String, String> mHash;
+
+    public UserLogin(String userID, String userPassword, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null); // 여기가 실질적으로 요청을 보내는 곳입니다.
+        mHash = new HashMap<>();
+        mHash.put("userID", userID);
+        mHash.put("userPassword", userPassword);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return mHash;
+    }
+}
