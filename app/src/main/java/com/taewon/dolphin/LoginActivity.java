d01fe8cac963e8d5ec2ff8458946d36292c484e6
DisIsGood <rlaxodnjs6574@gmail.com>
2021-05-15 18:48:52 +0900

    05.15 로그인폼 예시 작성
    1. font 폴더 추가

--- app/src/main/java/com/taewon/dolphin/LoginActivity.java
+++ app/src/main/java/com/taewon/dolphin/LoginActivity.java
@@ -1,12 +1,27 @@
 package com.taewon.dolphin;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText userID, userPassword;
+    Button loginBtn;
+    TextView register, find;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userID = (EditText) findViewById(R.id.userID);
+        userPassword = (EditText) findViewById(R.id.userPassword);
+        loginBtn = (Button)findViewById(R.id.loginBtn);
+        register = (TextView)findViewById(R.id.register);
+        find = (TextView)findViewById(R.id.find);
+
     }
 }
