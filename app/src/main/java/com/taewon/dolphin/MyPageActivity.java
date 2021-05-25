b28611b0343de8bd0d7aab663a4793353dea8539
hasinla <84169342+hasinla@users.noreply.github.com>
2021-05-26 01:41:28 +0900

    마이페이지
    구상중

--- app/src/main/java/com/taewon/dolphin/MyPageActivity.java
+++ app/src/main/java/com/taewon/dolphin/MyPageActivity.java
@@ -1,9 +1,13 @@
 package com.taewon.dolphin;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyPageActivity extends AppCompatActivity {
+    private TextView logOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
